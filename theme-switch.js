8076da3977c9bee69fde82b96571334ad967e415
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-02-25 15:16:18 +0330

    Delete some commented and unused code

--- theme-switch.js
+++ theme-switch.js
@@ -45,22 +45,7 @@ class ThemeSwitch extends HTMLElement {
 </button>
 `;
 
-        /*// Create (nested) span elements
-        const wrapper = document.createElement("span");
-        wrapper.setAttribute("class", "wrapper");
-        const icon = wrapper.appendChild(document.createElement("span"));
-        icon.setAttribute("class", "icon");
-        icon.setAttribute("tabindex", 0);
-        // Insert icon from defined attribute or default icon
-        const img = icon.appendChild(document.createElement("img"));
-        img.src = this.hasAttribute("img") ? this.getAttribute("img") : "img/default.png";
-
-        const info = wrapper.appendChild(document.createElement("span"));
-        info.setAttribute("class", "info");
-        // Take attribute content and put it inside the info span
-        info.textContent = this.getAttribute("data-text");*/
-
-        // Create some CSS to apply to the shadow dom
+        // Create some CSS to apply to the shadow DOM
         const style = document.createElement("style");
 
         // language=CSS
@@ -90,10 +75,6 @@ class ThemeSwitch extends HTMLElement {
 
         // attach the created elements to the shadow DOM
         this.shadowRoot.append(style/*, wrapper*/);
-    }
-
-    connectedCallback() {
-
     }
 }
 
