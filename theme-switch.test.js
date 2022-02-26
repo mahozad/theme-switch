f78505a05a57c59c459397ffbc3fced5bf8127c9
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-02-26 19:40:57 +0330

    Refactor code

--- theme-switch.test.js
+++ theme-switch.test.js
@@ -11,21 +11,7 @@ global.window = dom.window;
 global.document = window.document;
 global.HTMLElement = window.HTMLElement;
 global.localStorage = window.localStorage;
-
-// See https://stackoverflow.com/a/53449595/8583692
-Object.defineProperty(window, "matchMedia", {
-    writable: true,
-    value: jest.fn().mockImplementation(query => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // Deprecated
-        removeListener: jest.fn(), // Deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })),
-});
+setSystemThemeTo("light");
 
 const main = require("./theme-switch");
 
@@ -36,3 +22,22 @@ test("first test", () => {
 });
 
 console.log(`\u001B[32m✔️\u001B[39m Tests passed`);
+
+// See https://stackoverflow.com/a/53449595/8583692
+// and https://stackoverflow.com/a/57180950/8583692
+function setSystemThemeTo(mode) {
+    const isDark = mode === "dark";
+    Object.defineProperty(window, "matchMedia", {
+        writable: true,
+        value: jest.fn().mockImplementation(query => ({
+            matches: isDark,
+            media: query,
+            onchange: null,
+            addListener: jest.fn(), // Deprecated
+            removeListener: jest.fn(), // Deprecated
+            addEventListener: jest.fn(),
+            removeEventListener: jest.fn(),
+            dispatchEvent: jest.fn(),
+        })),
+    });
+}
