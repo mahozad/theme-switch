bcbe6ffcf2b3cedc645cde716427852c85be1252
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-03-12 17:36:21 +0330

    Add some options and comments in Rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,24 @@ export default {
     output: [
         {
             file: "dist/theme-switch.js",
-            format: "cjs"
+            /* amd, cjs, es, iife, umd, system */
+            format: "cjs",
+            strict: true,
         },
         {
             file: "dist/theme-switch.min.js",
             format: "iife",
+            validate: true,
+            /*
+             * "hidden" works like true except that the corresponding
+             * sourcemap comments in the bundled files are suppressed.
+             */
             sourcemap: true,
+            /*
+             * The global variable name representing your bundle.
+             * Other scripts on the same page can use this
+             * variable name to access the exports of our bundle.
+             */
             name: "themeSwitch",
             plugins: [terser()]
         }
