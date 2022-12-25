d8a47dd6a6077e75873931e69f0aaa05c7b3ece6
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-12-25 18:14:13 +0330

    Update highlightjs to version 11.7.0

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,4 +1,4 @@
-import highlightJs from "https://unpkg.com/@highlightjs/cdn-assets@11.4.0/es/highlight.min.js";
+import highlightJs from "https://unpkg.com/@highlightjs/cdn-assets@11.7.0/es/highlight.min.js";
 
 document.querySelectorAll("code")
     .forEach(element => {
