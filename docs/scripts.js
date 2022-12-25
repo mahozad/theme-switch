13a3e7b4980b0720715cfe9a362698f67e1f48ce
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2022-12-25 18:14:46 +0330

    Reformat a code

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,6 +1,7 @@
 import highlightJs from "https://unpkg.com/@highlightjs/cdn-assets@11.7.0/es/highlight.min.js";
 
-document.querySelectorAll("code")
+document
+    .querySelectorAll("code")
     .forEach(element => {
         highlightJs.highlightElement(element);
     });
